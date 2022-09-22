b8b5b443da767d77403ccf8038d2f7137cfdfd02
Gali Geller <gali1998@gmai.com>
2022-09-22 12:29:43 +0300

    fixed exp name

--- src/experiment.js
+++ src/experiment.js
@@ -93,7 +93,7 @@ export async function run({ assetPaths, input = {}, environment }) {
       let first_trial = jsPsych.data.get().values()[1];
       let participantId = first_trial["participantId"];
       
-      sendData("galit", "jspsych-try", jsPsych.data.get(), participantId);
+      sendData("galit", "audio-experiment", jsPsych.data.get(), participantId);
      }
   }
 
